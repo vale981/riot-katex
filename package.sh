9ce5352c6c929cbea3999ab3b87060398a720526
Valentin Boettcher <valentin@boettcher.cf>
2020-04-09 16:40:21 +0200

    bump version and refine packaging script

--- package.sh
+++ package.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 npm install
 
-mkdir katex-riot-pub
+mkdir -p katex-riot-pub
 cp manifest.json riot-katex.js node_modules/katex/dist/katex.min.js node_modules/katex/dist/contrib/auto-render.min.js node_modules/katex/dist/katex.min.css katex-riot-pub
-mkdir katex-riot-pub/fonts
+mkdir -p katex-riot-pub/fonts
 cp -r node_modules/katex/dist/fonts/*.woff2 katex-riot-pub/fonts/
 
-zip -r -FS katex-riot.zip katex-riot-pub/*
+cd katex-riot-pub/
+zip -r -FS ../katex-riot.zip *
